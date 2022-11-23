2d8557b844db9e34f64de71eaabf4496b8ba2a23
AdamChen <pn268268@gmail.com>
2022-11-23 11:58:00 +0800

    flip done

--- scrum.js
+++ scrum.js
@@ -2,3 +2,18 @@ const changeStep = (step) => {
     $('section').removeClass('active');
     $(`section.stage${step}`).addClass('active');
 }
+
+const filpCard = () => {
+    $('.stage3 .card-box').on("click", function () {
+        $(this).addClass('flip');
+
+        let count = $('.stage3 .card-box.flip').length
+        if (count == 3) {
+            $('.stage3 .next-btn').removeClass('hidden');
+        }
+    });
+}
+
+$(() => {
+    filpCard();
+});
