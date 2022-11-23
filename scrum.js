d49e370e8eb7c621efee54be21292d73661c8279
AdamChen <pn268268@gmail.com>
2022-11-23 16:55:05 +0800

    1-3 done

--- scrum.js
+++ scrum.js
@@ -1,6 +1,10 @@
 const changeStep = (step) => {
     $('section').removeClass('active');
     $(`section.stage${step}`).addClass('active');
+
+    if(step == 4){
+        step4fadeIn();
+    }
 }
 
 const filpCard = () => {
@@ -12,6 +16,12 @@ const filpCard = () => {
             $('.stage3 .next-btn').removeClass('hidden');
         }
     });
+}
+
+const step4fadeIn = () => {
+    $('.stage4 .card').eq(0).delay(0).animate({"opacity": "1"}, 1000);
+    $('.stage4 .card').eq(1).delay(700).animate({"opacity": "1"}, 1000);
+    $('.stage4 .card').eq(2).delay(1400).animate({"opacity": "1"}, 1000);
 }
 
 $(() => {
