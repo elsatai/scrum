ebe4a30d70166a61894fdf0eeeabd48215bc895b
Elsa Tai <elsatai@BingHongdeMacBook-Pro.local>
2022-11-25 01:07:48 +0800

    google font, rename section, 7-4 html

--- scrum.js
+++ scrum.js
@@ -2,33 +2,33 @@ const changeStep = (step) => {
     $('section').removeClass('active');
     $(`section.stage${step}`).addClass('active');
 
-    if (step == 4) {
+    if (step == '1-3') {
         step4fadeIn();
     }
 }
 
 const filpCard = () => {
-    $('.stage3 .card-box').on("click", function () {
+    $('.stage1-1 .card-box').on("click", function () {
         $(this).addClass('flip');
 
-        let count = $('.stage3 .card-box.flip').length
+        let count = $('.stage1-1 .card-box.flip').length
         if (count == 3) {
-            $('.stage3 .next-btn').removeClass('hidden');
+            $('.stage1-1 .next-btn').removeClass('hidden');
         }
     });
 }
 
 const step4fadeIn = async () => {
-    await $('.stage4 .card').eq(0).animate({
+    await $('.stage1-3 .card').eq(0).animate({
         "opacity": "1"
     }, 800).promise();
-    await $('.stage4 .card').eq(1).animate({
+    await $('.stage1-3 .card').eq(1).animate({
         "opacity": "1"
     }, 800).promise();
-    await $('.stage4 .card').eq(2).animate({
+    await $('.stage1-3 .card').eq(2).animate({
         "opacity": "1"
     }, 800).promise();
-    $('.stage4 .next-btn').removeClass('hidden');
+    $('.stage1-3 .next-btn').removeClass('hidden');
 }
 
 $(() => {
